eef5266f94ebd74a95099f493223ab1e851e09b5
JasonAkiraJordan <jasonajordan0@gmail.com>
2020-11-17 18:01:46 -0500

    i tried

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,15 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new 
+  end 
+
+  def create 
+    Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    #session[:form_params] = params.inspect
+    redirect_to students_path 
+  end 
+
+
+
 end
